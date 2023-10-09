e1aa152febfb64862be097a321103601e11f38ed
KonstantinosChanioglou <konstantinosch@hotmail.co.uk>
2023-10-09 21:27:39 +0200

    Test commit

--- phase_1/client/client.py
+++ phase_1/client/client.py
@@ -7,7 +7,9 @@ conn = rpyc.connect("localhost", 18861)
 print("Connected")
 
 while True:
+    #test comment
     index = "".join(choices(ascii_uppercase, k=3))
     price = conn.root.get_price(index)
     print(f"Price of {index} is ${price:.2f}", flush=True)
     time.sleep(5)
+ 
