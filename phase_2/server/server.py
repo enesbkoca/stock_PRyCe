9c7aa9a7c9b4aa22d27a186d4432e81880ee630e
Berkay_HP <enesbkoca@gmail.com>
2023-10-21 19:47:46 +0200

    Working version of load balancing and statistics

--- phase_2/server/server.py
+++ phase_2/server/server.py
@@ -2,6 +2,7 @@ import rpyc
 import socket
 import os
 from api import get_price
+from rpyc.utils.server import ThreadedServer
 
 
 class StockPrice(rpyc.Service):
@@ -14,14 +15,14 @@ class StockPrice(rpyc.Service):
     def exposed_get_price(self, stock):
         # time.sleep(10)
 
-        stock_price = 10, "23:34"
-        # stock_price = get_price(stock)
+        # stock_price = 10, "23:34"
+        stock_price = get_price(stock)
         print("Returning {} @ {}".format(stock, stock_price[0]))
         return stock_price
 
 
 if __name__ == "__main__":
-    from rpyc.utils.server import ThreadedServer
+
 
     hostname = socket.gethostname()  # Get the hostname of the server
     ip_address = socket.gethostbyname(hostname)  # Get the IP address corresponding to the hostname
